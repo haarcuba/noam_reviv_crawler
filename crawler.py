d32e543d0857b8275cd33fcfbc12f926814e78b6
root <root@LAPTOP-7PI6R8OK>
2023-11-18 21:28:36 +0200

    version 6

--- crawler.py
+++ crawler.py
@@ -1,9 +1,13 @@
+'''
+This is Raviv's and Noam's project :)
+'''
+
 import argparse
 import urllib.request
 import yaml
 import re
 
-
+#gets input from user
 parser = argparse.ArgumentParser()
 parser.add_argument('-d', '--depth', type=int, default=2)
 parser.add_argument('-ir', '--ignore_regex', type=str, default="^$") #compatible only with the string: "", there is no url like this!
@@ -15,21 +19,11 @@ regex = arguments.ignore_regex
 url = arguments.url
 
 
-"""
-make it look good
-"""
-
-
-
-def make_dict(url, depth):
-    temp = [{"level":i, "links":[]} for i in range(depth+1)]
-    output = {"root":url, "web":temp}
-    output["web"][0]["links"] = [url]
-    return output
-
-
-
 def scan_urls_href(html_code, src):
+    '''
+    return a list of all the urls from a url (html_code)
+    using attribute src
+    '''
     last_index = 0
     urls_lst = []
     while last_index < len(html_code):
@@ -41,6 +35,7 @@ def scan_urls_href(html_code, src):
                 lst.append(html_code[j])
                 j += 1
             last_index = j + 1
+            #makes the links readable by the library
             if lst[0] == '/' and lst[1] != '/':
                 link = src + "".join(lst)
             elif lst[0] == '/' and lst[1] == '/':
@@ -57,7 +52,11 @@ def scan_urls_href(html_code, src):
     return urls_lst
 
 
-def scan_urls_src(html_code, src):
+def scan_urls_src(html_code, src, new_lst):
+    '''
+    return a list of all the urls from a url (html_code)
+    using attribute src
+    '''
     last_index = 0
     url_lst = []
     while last_index < len(html_code):
@@ -69,6 +68,7 @@ def scan_urls_src(html_code, src):
                 lst.append(html_code[j])
                 j += 1
             last_index = j + 1
+            #makes the links readable by the library
             if lst[0] == '/' and lst[1] != '/':
                 link = src + "".join(lst)
             elif lst[0] == '/' and lst[1] == '/':
@@ -78,34 +78,46 @@ def scan_urls_src(html_code, src):
                     link = 'http:' + "".join(lst)
             else:
                 link = "".join(lst)
-            if link not in urls_lst:
+            if link not in urls_lst and link not in new_lst:
                 urls_lst.append(link)
         else:
             last_index = len(html)
     return urls_lst
 
 
-
-
 def get_all_urls(regex, depth, url):
-    dictionary = make_dict(url, depth)
+    '''
+    the main function
+    defines the dictionary
+    adds the urls
+    converts to yaml
+    '''
+    temp = [{"level":i, "links":[]} for i in range(depth+1)]
+    dictionary = {"root":url, "web":temp}
+    dictionary["web"][0]["links"] = [url]
+
     for i in range(1, depth+1):
         lst = []
         for j in dictionary["web"][i-1]["links"]:
+            #try so we dont get a 404/403 error
             try:
                 if not re.search(regex, j):
-                    web_url = urllib.request.urlopen(j)
-                    data = str(web_url.read())
+                    data = str(urllib.request.urlopen(j).read())
                     src_site = get_site_name(j)
                     lst += scan_urls_href(data, src_site)
-                    lst += scan_urls_src(data, src_site)
+                    lst += scan_urls_src(data, src_site, lst)
             except:
                 continue
         dictionary["web"][i]["links"] = lst
+
     return yaml.dump(dictionary)
 
 
 def get_site_name(url):
+    '''
+    get the name of the parent site.
+    example: https://en.wikipedia.org/wiki/Chess => https://en.wikipedia.org
+    '''
     counter = 0
     index = 0
     while index < len(url):
@@ -116,13 +128,7 @@ def get_site_name(url):
         index += 1
     return url
 
-
-#The input must be http / https site because of package limitations.
+#calls the main funtion and prints the yaml
+#doesn't work for non http/https site because of library limitations
 print(get_all_urls(regex, depth, url))
-
-
-
-
-
-
 
